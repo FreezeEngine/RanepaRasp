ed7408623fee44078d3d1de18f99506ef32bbf89
FreezeEngine <mr.springtrap@yandex.ru>
2020-10-15 01:41:55 +0300

    Add files via upload

---
+++ precache-manifest.52b589a9d959b975db4b33369f7594ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9bbb03785b51036a08c409567db1ec3",
+    "url": "/RanepaRasp/index.html"
+  },
+  {
+    "revision": "1bf2045285a759c1bfd9",
+    "url": "/RanepaRasp/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "3e9a076867278e2db59f",
+    "url": "/RanepaRasp/static/css/main.e9f6f6e1.chunk.css"
+  },
+  {
+    "revision": "1bf2045285a759c1bfd9",
+    "url": "/RanepaRasp/static/js/2.cf9e1474.chunk.js"
+  },
+  {
+    "revision": "b6f931465f427c7257a302feb961d3b5",
+    "url": "/RanepaRasp/static/js/2.cf9e1474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9a076867278e2db59f",
+    "url": "/RanepaRasp/static/js/main.952d0708.chunk.js"
+  },
+  {
+    "revision": "4e6a1416d3da8a2d8d72",
+    "url": "/RanepaRasp/static/js/runtime-main.e91c004f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/raneparasp/precache-manifest.d5c18042d2e7762be1a4c324816d5efa.js"
+  "/RanepaRasp/precache-manifest.52b589a9d959b975db4b33369f7594ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raneparasp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RanepaRasp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
